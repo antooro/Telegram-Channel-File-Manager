7c6484aa0722198d0f1feae4940959c78638bb21
antooro <34341810+antooro@users.noreply.github.com>
2018-10-10 21:06:04 +0200

    Log sys
    Añadido sistema de logs, para notificar las novedades

--- enviar.py
+++ enviar.py
@@ -8,45 +8,34 @@ import os
 from config import *
 import filetype
 import urllib
+nuevos = 'log'+CANAL.replace('@','')+'.txt'   
+try:
+    with open(nuevos, 'r') as f:
+        old = f.read().split('\n')
+except:
+    old = []
 
+new = {}
+nov = []
+fich = []
 ides = []
 asigs = {}
 subcat = {}
 bot = telebot.TeleBot(BOT_APIKEY)
 os.chdir(CARPETA)
-
+d = 0
 for root, dirs, files in os.walk("./"):  
+    
+          
     for dir in dirs:
-        if root == './' and len(files) is not 0:
-            s = bot.send_message(CANAL,"Archivos sueltos")
-            ides.append(s.message_id)
-            asigs["Archivos sueltos"]= s.message_id
-            for n in files:
-                kind = filetype.guess(root+n)
-                if kind is not None:
-                    abcd = kind.mime.split("/")
-                    if abcd[0] == "video":
-                        s = bot.send_video(CANAL,open(n,'rb'))
-                        ides.append(s.message_id)
-                    elif abcd[0] == "audio":
-                        s = bot.send_audio(CANAL,open(n,'rb'))
-                        ides.append(s.message_id)
-                    elif abcd[0] == "image":
-                        s = bot.send_photo(CANAL,open(n,'rb'))
-                        ides.append(s.message_id)
-                    else:
-                        s = bot.send_document(CANAL,open(n,'rb'))
-                        ides.append(s.message_id)
-                else:
-                        s = bot.send_document(CANAL,open(n,'rb'))
-                        ides.append(s.message_id)
-                
+        
         for root1,dirs1,files1 in os.walk(dir):
             
             if len(files1) is not 0 or len(dirs1) is not 0:
                 
                 s = bot.send_message(CANAL,(root1.replace("\\"," ")))
                 ides.append(s.message_id)
+                
                 if '\\' not in root1:  
                     asigs[root1.replace("\\"," ")]= s.message_id
                 else:
@@ -55,25 +44,64 @@ for root, dirs, files in os.walk("./"):
                 root1 = '/'.join(root1.split('\\'))
                 a =("./"+ root1+"/"+f3)
                 kind = filetype.guess(a)
+                fich.append(f3)
+                
+                
                 if kind is not None:
                     abcd = kind.mime.split("/")
                     if abcd[0] == "video":
                         s = bot.send_video(CANAL,open(a,'rb'))
                         ides.append(s.message_id)
+                        if f3 not in old and old: new[f3] = s.message_id
                     elif abcd[0] == "audio":
                         s = bot.send_audio(CANAL,open(a,'rb'))
                         ides.append(s.message_id)
+                        if f3 not in old and old: new[f3] = s.message_id
                     elif abcd[0] == "image":
                         s = bot.send_photo(CANAL,open(a,'rb'))
                         ides.append(s.message_id)
+                        
+                        if f3 not in old and old: new[f3] = s.message_id
                     else:
                         s = bot.send_document(CANAL,open(a,'rb'))
                         ides.append(s.message_id)
+                        if f3 not in old and old: new[f3] = s.message_id
                 else:
                         s = bot.send_document(CANAL,open(a,'rb'))
                         ides.append(s.message_id)
-
+                        if f3 not in old and old: new[f3] = s.message_id
+    if root == './' and len(files) is not 0 :
+            s = bot.send_message(CANAL,"Archivos sueltos")
+            ides.append(s.message_id)
+            asigs["Archivos sueltos"]= s.message_id
+            for n in files:
+                kind = filetype.guess(root+n)
+                fich.append(n)
+                
+                if kind is not None:
+                    abcd = kind.mime.split("/")
+                    if abcd[0] == "video":
+                        s = bot.send_video(CANAL,open(n,'rb'))
+                        ides.append(s.message_id)
+                        if n not in old and old: new[n]=s.message_id
+                    elif abcd[0] == "audio":
+                        s = bot.send_audio(CANAL,open(n,'rb'))
+                        ides.append(s.message_id)
+                        if n not in old and old: new[n]=s.message_id
+                    elif abcd[0] == "image":
+                        s = bot.send_photo(CANAL,open(n,'rb'))
+                        ides.append(s.message_id)
+                        if n not in old and old: new[n]=s.message_id
+                    else:
+                        s = bot.send_document(CANAL,open(n,'rb'))
+                        ides.append(s.message_id)
+                        if n not in old and old: new[n]=s.message_id
+                else:
+                        s = bot.send_document(CANAL,open(n,'rb'))
+                        ides.append(s.message_id)
+                        if n not in old and old: new[n] = s.message_id
 a=[]
+
 markup = types.InlineKeyboardMarkup()
 for key,value in asigs.iteritems():
     markup.row(types.InlineKeyboardButton(str(key) ,url=LINK_CANAL+"/"+str(value)))
@@ -86,8 +114,22 @@ for key,value in asigs.iteritems():
 a = bot.send_message(CANAL,"MENU", reply_markup=markup,parse_mode='Markdown')
 ides.append(a.message_id)
 
+if new:
+    mensaje = 'Nuevos archivos\n\n'
+
+    for archivo, mid in new.iteritems():
+        mensaje += u"[{}]({}/{})".format(archivo,LINK_CANAL,mid)+"\n"
+        
+        
+    bot.send_message(CANAL,mensaje,parse_mode='Markdown',disable_web_page_preview=True)
+
+
 os.chdir('..')
 a = 'ides'+CANAL.replace('@','')+'.txt'
 with open(a, 'w') as f:
     for item in ides:
+        f.write("%s\n" % item)
+
+with open(nuevos, 'w') as f:
+    for item in fich:
         f.write("%s\n" % item)
